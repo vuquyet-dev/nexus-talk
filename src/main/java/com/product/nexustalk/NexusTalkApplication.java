77bf527f92c30e47b110e22c71d4083e6c42b730
quyettts <quyettts@gmail.com>
2026-04-20 16:46:47 +0700

    init project

---
+++ src/main/java/com/product/nexustalk/NexusTalkApplication.java
@@ -0,0 +1,13 @@
+package com.product.nexustalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/product/nexustalk/NexusTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.nexustalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
